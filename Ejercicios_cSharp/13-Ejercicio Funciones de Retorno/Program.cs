a1b1d7212d3fe1aa73047cf0ed267a5808866003
Roberto Torres <emiltores15@gmail.com>
2020-10-25 23:05:22 -0600

    Modificando ejercicio 13

---
+++ Ejercicios_cSharp/13-Ejercicio Funciones de Retorno/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno(1, "Juan", "Perez");
+
+            Console.WriteLine(a.nombreCompleto());
+
+
+        }
+    }
+}
